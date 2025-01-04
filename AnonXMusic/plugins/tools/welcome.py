c091cc214f6d532157b7e8b44eee81671d2e1df3
MISHRAJII123 <ccaddnew@gmail.com>
2025-01-04 20:23:37 +0530

    Update welcome.py

--- AnonXMusic/plugins/tools/welcome.py
+++ AnonXMusic/plugins/tools/welcome.py
@@ -90,11 +90,9 @@ async def greet_new_member(_, member: ChatMemberUpdated):
     
     try:
         # Welcome message
-        welcome_message = f"👋 {user.mention}, Wᴇʟᴄᴏᴍᴇ Tᴏ {member.chat.title}!\n\n" \
-                          "• I Hᴏᴘᴇ Yᴏᴜ Aʀᴇ Fɪɴᴇ!\n\n" \
-                          "• Pʟᴇᴀsᴇ Aʟᴡᴀʏs Fᴏʟʟᴏᴡ Tʜᴇ Gʀᴏᴜᴘ Rᴜʟᴇs!\n" \
+        welcome_message = f"𝙃ᴇʏ 𝘽ᴀʙʏ {user.mention}, 𝙒єℓ¢σмє {member.chat.title}!\n\n" \
                           "────────────────────\n" \
-                          f"ᴛ ᴏ ᴛ ᴀ ʟ ᴍ ᴇ ᴍ ʙ ᴇ ʀ: {count}\n" \
+                          f"𝘼ℓℓ 𝙈ємвєя : {count}\n" \
                           "────────────────────**"
         
         # Creating an inline button to "Join 👋" with the link
