56e5ef007513c8c9d8aa156a28405148c9762a03
NAWAB-MUSIC <kapilpandit9560@gmail.com>
2025-03-03 02:01:09 +0530

    Update welcome.py

--- AnonXMusic/plugins/tools/welcome.py
+++ AnonXMusic/plugins/tools/welcome.py
@@ -97,7 +97,7 @@ async def greet_new_member(_, member: ChatMemberUpdated):
         
         # Creating an inline button to "❍ 𝙅𝙊𝙄𝙉 ❍" with the link
         keyboard = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("❍ 𝙅𝙊𝙄𝙉 ❍", url="https://t.me/+Sa6dJpmZECBmNDU9")]]
+            [[InlineKeyboardButton("❍ 𝙅𝙊𝙄𝙉 ❍", url="https://t.me/+VWjyWWYcC4hjOGVl")]]
         )
 
         # Send the welcome message with the inline button
